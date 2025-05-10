ec7729841733045702297a7a688b694514e83cc2
kille-sudo <eh.me.1389.13@gmail.com>
2025-05-10 16:28:58 +0330

    bot.py
    update

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import telebot
 import yt_dlp
 import os
-from youtubesearchpython import VideosSearch
 
 TOKEN = '7909038781:AAHpi139qdAatRgj0OvQVmIDjDsHhCcuhW8'
-
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -12,34 +10,25 @@ def welcome(message):
     bot.send_message(message.chat.id, "سلام! اسم آهنگ رو بفرست تا از یوتیوب برات MP3 بفرستم.")
 
 @bot.message_handler(func=lambda message: True)
-def get_audio(message):
-    query = message.text
-    bot.send_message(message.chat.id, f"در حال جستجوی: {query}")
-
-    videosSearch = VideosSearch(query, limit=1)
-    results = videosSearch.result()
-    if not results['result']:
-        bot.send_message(message.chat.id, "آهنگی پیدا نشد.")
-        return
-
-    video_url = results['result'][0]['link']
-    title = results['result'][0]['title']
-    bot.send_message(message.chat.id, f"دانلود از: {title}")
+def download_mp3(message):
+    query = f"ytsearch:{message.text}"
 
     ydl_opts = {
         'format': 'bestaudio/best',
+        'noplaylist': True,
         'outtmpl': 'song.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'quiet': True
+        'quiet': True,
     }
 
+    bot.send_message(message.chat.id, f"در حال جستجو و دانلود: {message.text}")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video_url])
+            ydl.download([query])
         audio = open('song.mp3', 'rb')
         bot.send_audio(message.chat.id, audio)
         audio.close()
